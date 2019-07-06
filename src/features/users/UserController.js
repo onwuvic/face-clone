701e19f03307df73e55fa8fd008ccb9197820e98
Onwuzor Victor <victor.onwuzor@andela.com>
2019-07-06 15:49:26 +0100

    feat: basic endpoint settings this PR is for basic setups
    - create user controller and service
    - create profile controller and service
    - create routes for both user and profile

---
+++ src/features/users/UserController.js
@@ -0,0 +1,7 @@
+class UserController {
+  static create(req, res) {
+    res.json('User Created');
+  }
+}
+
+export default UserController;
---
+++ src/routes/index.js
@@ -0,0 +1,11 @@
+import userRouter from '../features/users/route';
+import profileRouter from '../features/profiles/route';
+
+const apiPrefix = '/api/v1';
+
+const routes = (app) => {
+  app.use(apiPrefix, userRouter);
+  app.use(apiPrefix, profileRouter);
+};
+
+export default routes;
